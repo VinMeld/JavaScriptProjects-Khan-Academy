845dbefc394ea9a5adcb488d118c54eabaefb131
Vinaymeldrum <36042425+Vinaymeldrum@users.noreply.github.com>
2019-03-05 14:37:04 -0500

    Added dinner

---
+++ Project:What'sforDinner?.js
@@ -0,0 +1,11 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); //plate
+fill(184, 0, 184);
+ellipse(200,200,100,100); //Blueberry
+fill(184, 0, 184);
+ellipse(300,200,100,100); //Some more blueberry
+fill(33, 4, 181);
+rect(200,200,100,100); //Some icecream
+fill(237, 0, 0);
+rect(100,200,100,100); //Strawberry
